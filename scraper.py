88664b2eee5ae6b12538f9bb814065b453c1e2ae
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-10-15 16:53:09 -0500

    Adding basic info about scraper.

--- scraper.py
+++ scraper.py
@@ -1,11 +1,7 @@
-# MN 2012 General Election Results
-
+"""
 Scraper for the 2012 general election in Minnesota.  Main
 results page can be found here:
 
 http://electionresults.sos.state.mn.us/enr/ENR/Home/1
 http://electionresults.sos.state.mn.us/ENR/Select/Download/1
-
-## Scraper
-
-https://box.scraperwiki.com/zzolo/mn-2012-election-results
+"""
